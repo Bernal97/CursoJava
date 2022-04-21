7915de9ba33b437beb111a16f59ca6e99fd510f5
Bernal97 <84948951+Bernal97@users.noreply.github.com>
2022-04-21 17:37:29 -0300

    Add files via upload

---
+++ dia12/apiRest/src/main/java/com/example/demo/model/Persona.java
@@ -0,0 +1,63 @@
+package com.example.demo.model;
+
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+import lombok.Data; //Esto sirve para no escribir getter y setter
+
+
+@Document(value = "Personas")
+@Data
+public class Persona {
+	@Id
+	private String id;
+	private String nombres;
+	private String apellidos;
+	private String email;
+	private int edad;
+	
+	
+	
+	
+	
+	public Persona(String id, String nombres, String apellidos, String email, int edad) {
+		super();
+		this.id = id;
+		this.nombres = nombres;
+		this.apellidos = apellidos;
+		this.email = email;
+		this.edad = edad;
+	}
+	public String getId() {
+		return id;
+	}
+	public void setId(String id) {
+		this.id = id;
+	}
+	public String getNombres() {
+		return nombres;
+	}
+	public void setNombres(String nombres) {
+		this.nombres = nombres;
+	}
+	public String getApellidos() {
+		return apellidos;
+	}
+	public void setApellidos(String apellidos) {
+		this.apellidos = apellidos;
+	}
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	public int getEdad() {
+		return edad;
+	}
+	public void setEdad(int edad) {
+		this.edad = edad;
+	}
+	
+	
+}
